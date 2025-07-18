ee3099b26747a0bd65171408b57b90d231cea596
kadoglou <konstantinos.kadoglou@pm.me>
2025-07-18 14:04:31 +0300

    Fix. iOS example wasn't executing because of a typo.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ComposeApp
+import composeapp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
